c286e3b48ca962e844fbce2e85860c0ed96ef957
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-02 10:50:14 -0300

    Add files via upload

---
+++ victor/idade.js
@@ -0,0 +1,6 @@
+let idade = 18;
+let dataNascimento = 2008;
+
+//console.log("Minha idade é: " + idade + " anos");
+//console.log("Meu ano de nascimento é: " + dataNascimento);
+console.log(`Minha idade é: ${idade} e minha data de nascimento é: ${dataNascimento}`);
