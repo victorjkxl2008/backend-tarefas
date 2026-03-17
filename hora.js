1c12ca0ac8d9e2ae5a6c0b8d32d7692bac6cd847
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-17 11:55:14 +0000

    1

--- hora.js
+++ hora.js
@@ -3,18 +3,18 @@ let valorPorHora = 50;
 let horasTrabalhadas = 8; // Expediente padrão
 
 console.log("--- Resumo do Expediente");
-console.log("Horas iniciais: $(horasTrabalhadas]h');
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
 
 horasTrabalhadas++;
 console.log(`Após hora extra (horasTrabalhadas++): $(horas Trabalhadash"`);
 
 //No dia seguinte ele precisou sair uma hora mais cedo para ir ao médico
 horasTrabalhadas--
-console.log("Após saida antecipada (horas Trabalhadas--): $(horas Trabalhadash);
+console.log(`Após saida antecipada (horas Trabalhadas--): ${horasTrabalhadas}h`);
 
 //Cálculo final
 let taxaSistema = 15;
-let pagamentoTotal (horasTrabalhadas * valorPorHora) - taxasistema;
+let pagamentoTotal = (horasTrabalhadas * valorPorHora) - taxasistema;
 
-console.log("/n--- Fechamento);
+console.log("/n--- Fechamento");
 console.log(`Total a receber por ${horasTrabalhadas}h: R$ ${pagamentoTotal}`);
