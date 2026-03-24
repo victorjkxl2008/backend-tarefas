7b1bb2ae7bd36cde1b0b0ba44a7da8fbfb49871c
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-24 13:44:43 +0000

    versao4

---
+++ stringo.js
@@ -0,0 +1,10 @@
+console.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua das Flores";
+let numero = 9851;
+let cidade = "Curitiba";
+let cep = "80010-000";
+
+let enderecoCompleto = rua + "," + numero + "-" + cidade;
+
+console.log(enderecoCompleto);
