6561ea4e14b98442c064ca567f6f68046916cc6d
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-24 13:54:17 +0000

    versao4

---
+++ atributo.js
@@ -0,0 +1,18 @@
+let numero = 2;
+numero = numero * 2;
+
+console.log(numero);
+
+let numeroN = 2;
+numeroN /= 2;
+
+console.log(numeroN);
+
+const idadeNeymar = 34;
+const minhaIdade = 18;
+
+const saoIguais = idadeNeymar == minhaIdade;
+console.log('As idades são iguais? ' + saoIguais);
+
+const comparacao = idadeNeymar > minhaIdade;
+console.log('O Neymar é mais velho? ' + comparacao);
