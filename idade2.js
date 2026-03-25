6b4c9afef6e481404a5824a4643272ee874b5e85
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-25 12:37:58 +0000

    versao4

---
+++ idade2.js
@@ -0,0 +1,8 @@
+const nome = "Victor";
+const idade = 18;
+
+if (idade >= 18) {
+    console.log(nome + ", " + "Ele é maior de idade.")
+} else {
+    console.log(nome + ", " + "é menor de idade")
+};
