0fd4100b2f811e1225117fe5b176d7db073d32ac
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-31 14:31:34 +0000

    versao4

--- cnh.js
+++ cnh.js
@@ -1,3 +1,12 @@
 const idade = 18;
 const maiorIdade = idade >= 18;
 const possuiCNH = false;
+
+const podeDirigir = Maioridade && possuiCNH;
+console.log('Pode viajar sozinho?', podeDirigir)
+
+const podeViajarSozinho = Maioridade || possuiCNH;
+console.log ('Pode viajar sozinho?', podeViajarSozinho);
+
+const precisaDeAcompanhante = !Maioridade
+console.log('Preciso de Acompanhante?', precisaDeAcompanhante);
