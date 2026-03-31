77dc29be914ecca48714d8e728490811385ead39
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-31 14:44:27 +0000

    versao4

--- venda.js
+++ venda.js
@@ -7,3 +7,9 @@ let nomeProduto = "Teclado Mecânico";
 let precoProduto = 200.00;
 let estoque = 10;
 let quantidadePedido = 5;
+
+let estoqueOK = quantidadePedido <= estoque;
+
+if (!estoqueOK) {
+    console.log("Pedido RECUSADO - estoque insuficiente")
+};
