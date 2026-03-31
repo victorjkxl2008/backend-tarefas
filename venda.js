c6599f97b1f90e7865376656216f51e5752befca
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-03-31 14:32:18 +0000

    versao4

---
+++ venda.js
@@ -0,0 +1,9 @@
+//--Dados do Pedido--
+
+let nomeCliente = "Victor André";
+let tipoCliente = "premium";
+
+let nomeProduto = "Teclado Mecânico";
+let precoProduto = 200.00;
+let estoque = 10;
+let quantidadePedido = 5;
