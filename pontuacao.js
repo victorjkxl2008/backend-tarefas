260f8410067762cef2dc3978bcd0854105063e07
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-04-07 11:50:35 +0000

    versao4

---
+++ pontuacao.js
@@ -0,0 +1,8 @@
+const nomeJogador = "Myball";
+let faseAtual = 4;
+let vidasRestantes = 3;
+
+let pontosPorMissao = [86, 91, 61, 101];
+
+console.log(`Seja bem vindo ${nomeJogador}!`);
+console.log(pontosPorMissao);
