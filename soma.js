2c2de8dc6d17ea623f6664b4f99b5e4b45f8e928
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-04-07 11:39:21 +0000

    versao4

---
+++ soma.js
@@ -0,0 +1,14 @@
+function somar (numero1, numero2) {
+return numero1 + numero2
+}
+console.log(somar (5,2));
+
+function verificarPar (numero) {
+    if (numero % 2 === 0){
+       return "O número é par"
+    } else {
+        return "O número é ímpar"
+    }
+};
+
+console.log(verificarPar(8));
