2906e636c6c51c1ea1748eb797237b4dcfef5351
victorjkxl2008 <v.cruz18@escola.pr.gov.br>
2026-04-27 11:54:19 +0000

    versao4

---
+++ objeto.js
@@ -0,0 +1,21 @@
+const bancoAutomotivo = {
+    material: "Couro Natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluido: false
+}
+
+console.log('---Detalhes do Banco---');
+console.log("Material escolhido: ", bancoAutomotivo.material);
+
+
+const produto = {
+    nome: "iPhone 25 Pro Max Ultra MegaBlaster",
+    marca: "Apple",
+    preco: 9299.90,
+    emEstoque: true,
+    especificacoes: ["A17 Pro chip", "Câmera 48MP", "Titânio"]
+};
+
+console.log('---Detalhes iPhone---');
+console.log("Preço do produto: ", produto.preco);
